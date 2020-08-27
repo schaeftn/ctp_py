eb564538efcff058f13b1a2c661f798d96dd4e21
Tristan Schäfer <tristan.schaefer@udo.edu>
2020-08-27 20:19:30 +0200

    changed result decoding to new data format

--- src/org/combinators/ctp/py/optimization/mqttClient.py
+++ src/org/combinators/ctp/py/optimization/mqttClient.py
@@ -1,12 +1,15 @@
+import asyncio
+
 import paho.mqtt.client as mqtt
 import sys
 import json
+from io import StringIO
 
 sys.path.append('/home/tristan/projects/ctp_py/src')
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print(f"Connected with result code {str(rc)}")
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
@@ -14,34 +17,44 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg, future, asyncloop):
-    print(msg.topic+" "+f""""{str(msg.payload.decode("utf-8") )}""")
+    print(msg.topic + " " + f""""{str(msg.payload.decode("utf-8"))}""")
 
     if msg.topic.startswith('bmResult'):
-        print(f"""Decoding bmResult {str(msg.payload.decode("utf-8"))}""")
-        t = json.loads(str(msg.payload.decode("utf-8")))
+        msg_payload = str(msg.payload.decode("utf-8"))
+        print(f"""Decoding bmResult {msg_payload}""")
+        t = json.load(StringIO(msg_payload))
         print(f"""Done decoding bmResult.""")
         dct = {}
-        if t[0] == 0.0:
-            dct['pathlength'] = 5000000.0
+        print(f"""Decoded dict: {t}""")
+        if t['pathLengths']:
+            dct['pathlength'] = sum(t['pathLengths']) / len(t['pathLengths'])
         else:
-            dct['pathlength'] = t[0]
-        dct['computationtime'] = t[1]
-        dct['failures'] = t[2]
+            dct['pathlength'] = 5000000.0
+        if t['computationTimes']:
+            dct['computationtime'] = sum(t['computationTimes']) / len(t['computationTimes'])
+        else:
+            dct['computationtime'] = 5000000.0
+
+        dct['failures'] = float(t['failures'])
         print(f"returning dct: {dct}")
         asyncloop.call_soon_threadsafe(cb, future.set_result(dct))
-    else:
-        if msg.topic.startswith('bmInitResponse'):
-            print("aaa")
-            if str(msg.payload.decode("utf-8")).startswith('Success'):
-                print("mqtt Client: success, returning 1")
-                asyncloop.call_soon_threadsafe(cb, future.set_result(1))
-            else:
-                print("received failure message. Future remains open.")
-                asyncloop.call_soon_threadsafe(cb, future.set_result(0))
+    elif msg.topic.startswith('bmInitResponse'):
+        if str(msg.payload.decode("utf-8")).startswith('Success'):
+            print("mqtt Client: success, returning 1")
+            asyncloop.call_soon_threadsafe(cb, future.set_result(1))
+        else:
+            print("Benchmark init: received failure message. Discarding run.")
+            asyncloop.call_soon_threadsafe(cb, future.set_result(0))
+    elif msg.topic.startswith('bmStartResponse'):
+        print("Received startResponse.")
+        asyncloop.call_soon_threadsafe(cb, future.set_result(1))
+    elif msg.topic.startswith('bmGenericInputResponse'):
+        print("Received inputResponse.")
+        asyncloop.call_soon_threadsafe(cb, future.set_result(1))
 
 
 def cb(*args):
-    print(f"callback {args}")
+    ()
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
@@ -50,4 +63,4 @@ def cb(*args):
 
 def send_message(client, s, topic):
     print(f"Sending message. Topic: {topic}, payload: {s}, client: {client} ")
-    client.publish(topic, payload=s, qos=0, retain=False)
+    client.publish(topic, payload=s, qos=0, retain=True)
